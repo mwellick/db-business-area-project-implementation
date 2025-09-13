0ebc1d51d9320eebdd3029f1773a888db0a5a558
Misha Korotia <mr.anderson528491@gmail.com>
2025-09-13 19:47:00 +0300

    added helper filter_data func in insert_data.py

--- nosql_projects_db/scripts/insert_data.py
+++ nosql_projects_db/scripts/insert_data.py
@@ -2,8 +2,22 @@ import os
 import json
 
 from nosql_projects_db.engine.init_db import init_db
+from nosql_projects_db.scripts.constraints import can_order_project
 
 db = init_db()
+
+
+def filter_data(data):
+    filtered_data = []
+    for project in data:
+        customer_name = project.get("customer")
+        if can_order_project(db, customer_name):
+            filtered_data.append(project)
+        else:
+            print(f"This customer: {customer_name} can't order a new project: "
+                  f"{project.get('name')}, because he has unpaid one ")
+    data = filtered_data
+    return data
 
 
 def insert_collection(collection_name, filename):
@@ -13,5 +27,9 @@ def insert_collection(collection_name, filename):
 
     with open(file_path, "r", encoding="utf-8") as f:
         data = json.load(f)
-    db[collection_name].insert_many(data)
-    print(f"Inserted {collection_name} into MongoDB")
+
+    if collection_name == "projects":
+        data = filter_data(data)
+    if data:
+        db[collection_name].insert_many(data)
+        print(f"Inserted {collection_name} into MongoDB")
