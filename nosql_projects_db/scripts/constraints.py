f9860804a0e389145d381af3291a90b468c30d79
Misha Korotia <mr.anderson528491@gmail.com>
2025-09-13 20:19:13 +0300

    Added filer_reports constraint according to the project logic

--- nosql_projects_db/scripts/constraints.py
+++ nosql_projects_db/scripts/constraints.py
@@ -6,7 +6,6 @@ def can_order_project(db, customer_name):
     Small explanation how it works:
     This constraint will work only and if DB already has data to compare.
     If DB is empty - all data will be inserted,because constraint still don't have data to compare yet.
-
     """
 
     three_months = (datetime.now() - timedelta(days=90)).isoformat()
@@ -30,3 +29,19 @@ def can_order_project(db, customer_name):
     )
 
     return len(list(projects)) == 0
+
+
+def can_report(db, executor_id, report_date, next_report_hours):
+    """
+    The same logic as in an above constraint docstring
+    If DS's empty - may be added any report (according to time)
+    """
+
+    reports = db.reports.find(
+        {
+            "executor_id": executor_id,
+            "date": report_date
+        }
+    )
+    total_hours = sum(r.get("hours", 0) for r in reports)
+    return total_hours + next_report_hours <= 10
